fa194564c449291f6627930492ade7832ac65aba
lChap701 <lucas_chapman@stu.indianhills.edu>
2021-06-30 00:40:00 -0500

    Back End changes

--- index.js
+++ index.js
@@ -1,31 +1,52 @@
 const express = require("express");
 const app = express();
+
 const cors = require("cors");
 require("dotenv").config();
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 const GoogleImages = require("google-images");
+/*const google = new Scraper({
+  puppeteer: {
+    headless: true,
+  },
+});*/
 
 app.use(cors());
 
 // Loads CSS styles
 app.use(express.static(__dirname + "/public"));
 
-// Displays the HTML page
+// Displays the Image Search Abstraction Layer page
 app.get("/", (req, res) => {
-  // Displays the HTML page
   res.sendFile(__dirname + "/public/index.html");
 });
 
-// Sets up search client
-const client = new GoogleImages(process.env.CSE_ID, process.env.API_KEY);
+// Finds images and displays the results
+app.post("/query/:query", (req, res) => {
+  // Sets up search client
+  const client = new GoogleImages(process.env.CSE_ID, process.env.API_KEY);
 
-client.search("Steve Angello").then((images) => {
-  console.log(images);
+  // Searches for images using query
+  const options = { page: req.body.pages, safe: req.body["safe-search"] };
+  client.search(req.params.query, options).then((imgs) => {
+    res.json(imgs);
+  });
 });
 
-client.search("Steve Angello", { page: 2 });
+// Displays the Google Custom Search Engine page
+app.get("/cse", (req, res) => {
+  res.sendFile(__dirname + "/public/cse.html");
+});
 
-const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log(
-    "Your app is listening on port " + listener.address().port + "\n"
-  );
+/*(async () => {
+  const results = await google.scrape("banana", 200);
+  console.log("results", results);
+})();*/
+
+const listener = app.listen(process.env.PORT || 8080, () => {
+  console.log("Your app is listening on port " + listener.address().port);
 });
