48ea08635bd8be48c65c7b22bd8f89f7ef62f845
lChap701 <lucas_chapman@stu.indianhills.edu>
2021-07-03 00:34:35 -0500

    Updated JS script

--- public/js/formHandler.js
+++ public/js/formHandler.js
@@ -5,7 +5,8 @@ function submitHandler() {
   // Adds query to the action attribute
   const form = document.querySelector("form[name='query-form']");
   const query = document.querySelector("input[name='query']").value;
-  form.action += query;
+  const pages = document.querySelector("input[name='pages']").value;
+  form.action += query + "?page=" + pages;
 
   // Updates the safeSearch checkbox
   const safeSearch = document.querySelector("input[name='safe-seach']");
