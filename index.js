9a4117b410ab26286a2580af79bf17828f327bfa
lChap701 <lucas_chapman@stu.indianhills.edu>
2021-07-03 02:13:41 -0500

    Added blank line

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ const GoogleImages = require("google-images");
 app.use(cors());
 
 const mongoose = require("mongoose");
+
 // Connect to DB
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
