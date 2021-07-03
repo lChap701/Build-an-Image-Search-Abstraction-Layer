bc80085b73b41c31376a200c21a3ee100a590c6b
lChap701 <lucas_chapman@stu.indianhills.edu>
2021-07-03 02:09:39 -0500

    Setup DB and renamed pages parameter to page

--- index.js
+++ index.js
@@ -12,12 +12,52 @@ const GoogleImages = require("google-images");
 
 app.use(cors());
 
+const mongoose = require("mongoose");
+// Connect to DB
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+let db = mongoose.connection;
+
+// Displays connection errors
+db.on("error", console.error.bind(console, "connection error:"));
+
+// Schema
+const Schema = mongoose.Schema;
+let imageSchema = new Schema({
+  query: String,
+  images: [
+    {
+      type: { type: { type: String } },
+      width: Number,
+      height: Number,
+      size: Number,
+      url: String,
+      thumbnail: {
+        url: String,
+        width: Number,
+        height: Number,
+      },
+      description: String,
+      parentPage: String,
+    },
+  ],
+  page: Number,
+  safe: String,
+});
+
+// Model
+const Image = mongoose.model("Images", imageSchema);
+
 // Formats the IP Address
 app.listen(process.env.PORT, "0.0.0.0");
 
 // Logs requests and other information
 app.use(function middleware(req, res, next) {
-  const fullPath = req.query.page > 0 ? req.path + "?page=" + req.query.page : req.path;
+  const fullPath =
+    req.query.page > 0 ? req.path + "?page=" + req.query.page : req.path;
   console.log(req.ip + " - " + req.method + "  " + fullPath);
   next();
 });
@@ -30,33 +70,36 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
 });
 
-// Finds images and displays the results after submitting a form
-app.post("/query/:query", (req, res) => {
-  // Sets up search client
-  const client = new GoogleImages(process.env.CSE_ID, process.env.API_KEY);
+// Executes once connected to DB
+db.once("open", function () {
+  // Finds images and displays the results after submitting a form
+  app.post("/query/:query", (req, res) => {
+    // Sets up search client
+    const client = new GoogleImages(process.env.CSE_ID, process.env.API_KEY);
 
-  // Searches for images using query
-  const options = { page: req.body.pages, safe: req.body["safe-search"] };
-  client.search(req.params.query, options).then((imgs) => {
-    res.json(imgs);
+    // Searches for images using query
+    const options = { page: req.body.page, safe: req.body["safe-search"] };
+    client.search(req.params.query, options).then((imgs) => {
+      res.json(imgs);
+    });
   });
-});
 
-// Finds images via query strings
-app.get("/query/:query", (req, res) => {
-  // Sets up search client
-  const client = new GoogleImages(process.env.CSE_ID, process.env.API_KEY);
+  // Finds images via query strings
+  app.get("/query/:query", (req, res) => {
+    // Sets up search client
+    const client = new GoogleImages(process.env.CSE_ID, process.env.API_KEY);
 
-  // Searches for images using query
-  const options = { page: req.query.page };
-  client.search(req.params.query, options).then((imgs) => {
-    res.json(imgs);
+    // Searches for images using query
+    const options = { page: req.query.page };
+    client.search(req.params.query, options).then((imgs) => {
+      res.json(imgs);
+    });
   });
-});
 
-// Displays the most recent searches
-app.get("/recent/", (req, res) => {
-  res.send("You made it!");
+  // Displays the most recent searches
+  app.get("/recent/", (req, res) => {
+    res.send("You made it!");
+  });
 });
 
 // Displays the Google Custom Search Engine page
