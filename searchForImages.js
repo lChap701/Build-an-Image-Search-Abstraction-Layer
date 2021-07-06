660d5aeb62a9608aa4513d4fd00c51ae363993dd
lChap701 <lucas_chapman@stu.indianhills.edu>
2021-07-06 17:10:20 -0500

    Module for search for images

---
+++ searchForImages.js
@@ -0,0 +1,25 @@
+const SearchOptions = require("./searchOptions"); // for JSDoc comment
+require("dotenv").config();
+
+// Sets up image searches using my programmable search engine
+const GoogleImages = require("google-images");
+const client = new GoogleImages(process.env.CSE_ID, process.env.API_KEY);
+
+/**
+ * Module for searching for images
+ * @module ./searchForImages
+ *
+ * @param {Response<any, Record<string, any>, number>} res  Represents the respose that should occur
+ * @param {String} query    Represents the query to search for
+ * @param {SearchOptions} options  Represents search options
+ *
+ */
+module.exports = function searchForImages(res, query, options) {
+  // Searches for images using the query and selected options
+  client
+    .search(query, options)
+    .then((imgs) => {
+      res.json(imgs);
+    })
+    .catch((err) => console.log(err));
+};
