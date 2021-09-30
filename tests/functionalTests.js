bcd25253d8b0a222f0a9c8601487d2f7c5058d6c
lChap701 <chapmanlucas201@gmail.com>
2021-09-29 19:38:31 -0500

    Added all functional tests

--- tests/functionalTests.js
+++ tests/functionalTests.js
@@ -6,18 +6,9 @@ chai.use(chaiHttp);
 
 const app = require("../index");
 
-// Connect to DB
-const mongoose = require("mongoose");
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-let db = mongoose.connection;
-
 suite("Functional Tests", () => {
   suite("GET Tests", () => {
-    test("1)  GET / Test", () => {
+    test("1)  GET / Test", (done) => {
       chai
         .request(app)
         .get("/")
@@ -27,10 +18,11 @@ suite("Functional Tests", () => {
             res.text.match(/<title>Image Search Abstraction Layer<\/title>/),
             "response text should contain '<title>Image Search Abstraction Layer</title>'"
           );
+          done();
         });
     });
 
-    test("2)  GET /cse Test", () => {
+    test("2)  GET /cse Test", (done) => {
       chai
         .request(app)
         .get("/cse")
@@ -40,27 +32,36 @@ suite("Functional Tests", () => {
             res.text.match(/<title>Google Custom Search Engine<\/title>/),
             "response text should contain '<title>Google Custom Search Engine</title>'"
           );
+          done();
         });
     });
 
-    /*test("3)  GET /query Test", () => {
+    test("3)  GET /query Test", (done) => {
       chai
         .request(app)
         .get("/query/test?page=1")
         .end((err, res) => {
           assert.equal(res.status, 200, "response status should be 200");
-          assert.isArray(res.body, "response body should be an array");
+          assert.isArray(
+            JSON.parse(res.text),
+            "response body should be an array"
+          );
+          done();
         });
     });
 
-    test("4)  GET /recent Test", () => {
+    test("4)  GET /recent Test", (done) => {
       chai
         .request(app)
         .get("/recent/")
         .end((err, res) => {
           assert.equal(res.status, 200, "response status should be 200");
-          assert.isArray(res.body, "response body should be an array");
+          assert.isArray(
+            JSON.parse(res.text),
+            "response body should be an array"
+          );
+          done();
         });
-    });*/
+    });
   });
 });
